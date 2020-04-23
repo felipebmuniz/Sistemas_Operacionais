61d363ce27f98ace617b4e63d5395d8b57bd2c4c
Felipe Barros Muniz <felipebmuniz@github.com>
2020-04-23 17:30:35 -0300

    Envio da atividade 1

---
+++ Threads/Atv_1.c
@@ -0,0 +1,30 @@
+//@felipebmuniz
+#include <stdio.h>
+#include <pthread.h>
+
+void *cont(void *ard) {
+    int *v = (int *) ard;
+    int i;
+    if (*v == 0)
+    {
+        for (i = 1; i <= 500; i++) {
+            printf("%d\n", i);
+        }
+    } else
+    {
+        for (i = 500; i > 0; i--) {
+            printf("%d\n", i);
+        }
+    }
+}
+
+int main() {
+    pthread_t thread1, thread2;
+    int c = 0;
+    int d = 1;
+    pthread_create(&thread1, NULL, cont, &c);
+    pthread_create(&thread2, NULL, cont, &d);
+
+    pthread_exit(NULL);
+    return 0;
+}
--- Threads/Exemplo.c
+++ Threads/Exemplo.c
@@ -3,28 +3,38 @@
 #include <pthread.h>
 #include <windows.h>
 
+int global = 0;
+
 void *func1(void *ard) {
-    int *v = (int *) ard;
-    printf("Chegou T1: %d\n", *v);
+    global++;
+    printf("T1: %d\n", global);
+    //int *v = (int *) ard;
+    //printf("Chegou T1: %d\n", *v);
 }
 
 void *func2(void *ard) {
-    char *nome = (char *) ard;
-    Sleep(2000);
-    printf("Nome T2: %s \n", nome);
+    global++;
+    printf("T2: %d\n", global);
+    //char *nome = (char *) ard;
+    //Sleep(2000);
+    //printf("Nome T2: %s \n", nome);
 }
 
 int main() {
-    pthread_t thread1, thread2;
+    pthread_t thread1[5], thread2[5];
 
-    int v = 10;
-    char nome[10] = "Felipe";
-    pthread_create(&thread1, NULL, func1, &v);
-    pthread_create(&thread2, NULL, func2, &nome);
+    //int v = 10;
+    //char nome[10] = "Felipe";
 
-    printf("Main\n");
-
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+        pthread_create(&thread1[i], NULL, func1, NULL);
+        pthread_create(&thread2[i], NULL, func2, NULL);
+    }
+    
     //pthread_join(thread1, NULL);
+    printf("Main\n");
     pthread_exit(NULL);
     return 0;
 }
