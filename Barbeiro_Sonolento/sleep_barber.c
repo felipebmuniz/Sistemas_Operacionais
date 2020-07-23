51a585ba6f98e00872452395f6fb35435f817a12
felipebmuniz <felipemuniz@alu.ufc.br>
2020-07-23 20:38:45 -0300

    reestruturação do código do barbeiro sonolento

--- Barbeiro_Sonolento/sleep_barber.c
+++ Barbeiro_Sonolento/sleep_barber.c
@@ -4,72 +4,76 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define cadeiras 5          // Define a quantidades de cadeiras na barbearia
+#define cadeiras 3          // Define a quantidades de cadeiras na barbearia
 #define num_clientes 10     // Define o numero de clientes
 #define true 1              // Define valor 1 como verdadeiro
 
 pthread_t barbeiro;                 // Denominando a thread de barbeiro
 pthread_t clientes[num_clientes];   // Denominando a thread de clientes
 
-sem_t clientes_espera;        // Semáforo de espera de clientes
+sem_t clientes_espera;       // Semáforo de espera de clientes
+sem_t barbeiro_ocupado;      // Semáforo do barbeiro cortando o cabelo do cliente
 sem_t ocupado;               // Semáforo do barbeiro
-sem_t mutex;                  // Semáforo de exclução mútua
 
-int esperando = 0;            // Número de clientes em espera
+int id[num_clientes];        // Indentificador do número do cliente
 
-void* novo_barbeiro( void * args);
-void* novo_cliente( void * args);
+void *novo_barbeiro( void * args);
+void *novo_cliente( void * args);
 
 void main () {
-  sem_init(&ocupado, true, 0);           // Inicialização do semáforo
-  sem_init(&clientes_espera, true, 0);    // Inicialização do semáforo
-  sem_init(&mutex, true, 1);              // Inicialização do semáforo
+  
+  sem_init(&barbeiro_ocupado, 0, 1);           // Inicialização do semáforo
+  sem_init(&clientes_espera, 0, cadeiras);     // Inicialização do semáforo
+  sem_init(&ocupado, 0, 0);                       // Inicialização do semáforo
+  
 
-  pthread_create(&barbeiro, NULL, &novo_barbeiro, NULL);  // Criação do barbeiro
+  pthread_create(&barbeiro, NULL, novo_barbeiro, NULL);  // Criação do barbeiro
 
   int i;
   for (i = 0; i < num_clientes; i++)
   {
-    pthread_create(&clientes, NULL, &novo_cliente, NULL); // Criação dos clientes
-    sleep(rand()%100);
+    id[i] = i;
+    pthread_create(&clientes[i], NULL, novo_cliente, &id[i]); // Criação dos clientes
+    usleep(rand()%100);
   }
 
   for (i = 0; i < num_clientes; i++)
   {
-    pthread_join(&clientes[i], NULL); // Criação dos clientes
+    pthread_join(clientes[i], NULL); // Criação dos clientes
   }
 
-  printf('Fim do espediente do barbeiro.\n');
+  printf("Fim do espediente do barbeiro.\n");
 }
 
-void* novo_barbeiro( void * args) {
+void *novo_barbeiro( void * args) {
+  usleep(10);
   while (true)
   {
     sem_wait(&clientes_espera);   // Verifica o se há algum cliente em espera, se não vai dormir
-    sem_wait(&mutex);             // Verifica o espaço crítico
-    esperando--;                  // Diminui um cliente da fila de espera 
-    sem_post(&ocupado);           // Muda o estado do semáforo
-    sem_post(&mutex);             // Muda o estado do semáforo
-    printf('O barbeiro está cortando o cabelo de um cliente.\n');
+    printf("Barbeiro em atendimento.\n");
+    usleep(500);                  // realizando o atendimento
+    sem_post(&ocupado);           // Muda o estado do semáforo, sinaliza que o cabelo foi cortado
+
+    printf("O barbeiro está cortando o cabelo de um cliente.\n");
   }
   pthread_exit(NULL);             // Encerra a thread
 }
 
-void* novo_cliente( void * args) {
-  sem_wait(&mutex);   // Verifica o espaço crítico
-
-  if (esperando < cadeiras)
+void *novo_cliente( void * args) {
+  int id = *(int *) args;
+  usleep(id*2);
+  if (sem_trywait(&clientes_espera) == 0)
   {
-    printf('Um novo cliente chegou.\n');
-    esperando++;
-    sem_post(&clientes_espera);
-    sem_post(&mutex);
-    sem_wait(&ocupado);
-    printf('Cliente está cortando o cabelo.\n');
+    printf("Um novo cliente chegou: id_%d.\n", id);
+    sem_wait(&barbeiro_ocupado);                      // Aguarda o barbeiro
+    sem_post(&clientes_espera);                       // Um cliente é atendido
+    sem_wait(&ocupado);                               // Espera o corte de cabelo ser finalizado
+    printf("Cliente %d cortou o cabelo.\n", id);
+    sem_post(&clientes_espera);                       // Libera a cadeira de espera
+    sem_post(&barbeiro_ocupado);                      // Barbeiro liberado
   } else
   {
-    sem_post(&mutex);
-    printf('Barbearia lotada um possível cliente foi embora.\n');
+    printf("Barbearia lotada um possível cliente foi embora.\n");
   }
   pthread_exit(NULL);
 }
